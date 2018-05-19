d9b0888c454e82de5dcbec58dec64bd546005e64
linux_china <linux_china@hotmail.com>
2018-05-19 00:31:16 -0700

    spring boot application

---
+++ src/main/java/org/mvnsearch/App.java
@@ -0,0 +1,14 @@
+package org.mvnsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot application
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
