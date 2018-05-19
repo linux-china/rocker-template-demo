804dfcf228df85a137961c7ed6149acc71a681e5
linux_china <linux_china@hotmail.com>
2018-05-19 00:32:28 -0700

    Web Config to add rocker message converter

---
+++ src/main/java/org/mvnsearch/WebConfig.java
@@ -0,0 +1,22 @@
+package org.mvnsearch;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * Web Config to add rocker message converter
+ *
+ * @author linux_china
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.addDefaultHttpMessageConverters(converters);
+        converters.add(new RockerMessageConverter());
+    }
+}
