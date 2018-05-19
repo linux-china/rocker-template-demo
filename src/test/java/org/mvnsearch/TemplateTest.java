5a03691e1071481dacc24b199538fa8b25b9d7d2
linux_china <linux_china@hotmail.com>
2018-05-19 00:33:12 -0700

    template test

---
+++ src/test/java/org/mvnsearch/TemplateTest.java
@@ -0,0 +1,25 @@
+package org.mvnsearch;
+
+import com.fizzed.rocker.BindableRockerModel;
+import com.fizzed.rocker.Rocker;
+import com.fizzed.rocker.RockerModel;
+import org.junit.Test;
+
+/**
+ * template test
+ *
+ * @author linux_china
+ */
+public class TemplateTest {
+
+    /**
+     * template render test
+     */
+    @Test
+    public void testApp() {
+        RockerModel model = templates.Index.template("good", "title");
+        System.out.println(model.render().toString());
+        BindableRockerModel model2 = Rocker.template("templates/Index.rocker.html").bind("title", "welcome").bind("content", "demo");
+        System.out.println(model2.render().toString());
+    }
+}
