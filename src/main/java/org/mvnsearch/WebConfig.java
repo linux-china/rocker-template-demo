afbd4e5b21abb5fbb07db2c32353046087f8610d
linux_china <libing.chen@gmail.com>
2019-05-20 08:59:12 -0700

    implements WebMvcConfigurer

--- src/main/java/org/mvnsearch/WebConfig.java
+++ src/main/java/org/mvnsearch/WebConfig.java
@@ -2,7 +2,7 @@ package org.mvnsearch;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -12,11 +12,10 @@ import java.util.List;
  * @author linux_china
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new RockerMessageConverter());
-        super.addDefaultHttpMessageConverters(converters);
     }
 }
